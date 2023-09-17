42af7311295dba4fc4a9f6ce97b006ba2fdbb16c
niketrawat <144231088+niketrawat@users.noreply.github.com>
2023-09-17 16:59:29 +0530

    Update start.py

--- StrangerMusic/plugins/bot/start.py
+++ StrangerMusic/plugins/bot/start.py
@@ -41,7 +41,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAEDVvllBtgU28wKJ8jrNCL3ILF1th2ksAACRAoAAte3wVacWlXStgwbozAE")
+            await message.reply_sticker(
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"], reply_markup=keyboard
@@ -190,7 +190,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAEDVvllBtgU28wKJ8jrNCL3ILF1th2ksAACRAoAAte3wVacWlXStgwbozAE")
+                await message.reply_sticker(
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
